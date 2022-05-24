854e5779aa7629bbbb015a726acb6edb2ce17bd4
Imtiaz Azad <imtiaz16-549@diu.edu.bd>
2022-05-24 13:09:11 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        electrictheme: {
+          primary: "#72BBCA", 
+          secondary: "#557DA5",
+          accent: "#44497D",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
